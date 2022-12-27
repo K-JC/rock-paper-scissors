1c0780b2ca67bd8cdc837d9bfec4da1b43fc0cfc
K-JC <kellytolfts93@hotmail.com>
2022-12-27 22:04:07 +0000

    add eventlisteners for rock paper scissors button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,23 @@ const resultMessage = document.getElementById('result-message');
 const rockChoice = document.getElementById('rock');
 const paperChoice = document.getElementById('paper');
 const scissorsChoice = document.getElementById('scissors');
+/**
+ * create a function with event listener to listen for click of all buttons
+ */
+
+function game() {
+    rockChoice.addEventListener('click', function(){
+        startGame('rock');
+    })
+    
+    paperChoice.addEventListener('click', function(){
+        startGame('paper');
+    })
+    
+    scissorsChoice.addEventListener('click', function(){
+        startGame('scissors');
+    })
+    }
+    
+    game();
+    
