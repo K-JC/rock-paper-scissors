04334dfbbc13c2c7dad4691d8d4165d86eb902e4
K-JC <kellytolfts93@hotmail.com>
2022-12-27 21:50:34 +0000

    add some const variables to the js file

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * create variables for elements from the DOM
+*/
+
+const playerScore = document.getElementById('player-score');
+const computerScore = document.getElementById('computer-score');
+const resultMessage = document.getElementById('result-message');
+const rockChoice = document.getElementById('rock');
+const paperChoice = document.getElementById('paper');
+const scissorsChoice = document.getElementById('scissors');
