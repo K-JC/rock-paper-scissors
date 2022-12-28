f2bb46844044dc920c3aaf93fdece0978860fb45
K-JC <kellytolfts93@hotmail.com>
2022-12-28 18:57:42 +0000

    add a switch case statement for possible win loose solutions

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,19 +29,36 @@ function game() {
     game();
     
 /***
- * player choice function choice between rock,paper,scissors
+ * player choice function choice between rock,paper,scissors.
  */
 function startGame(playerChoice) {
-    const computerResult = computersChoice();
+    const computersChoice = theComputersChoice();
+    switch (playerChoice + computersChoice) {
+        case 'rockscissors':
+        case 'scissorspaper':
+        case 'paperrock':
+        console.log('You win yay!'); 
+        break;
+        case 'scissorsrock':
+        case 'paperscissors':
+        case 'rockpaper':
+            console.log('oh no the computer wins this time!');
+             break; 
+            case 'rockrock':
+            case 'paperpaper':
+            case 'scissorsscissors':
+                console.log('awwh man it is a draw!');
+                  break; 
+    }
     }
 
-    startGame('rock');
+    startGame();
 
  /**
  * computer choice function, random number between 1 and 3
 */
 
-function computersChoice() {
+function theComputersChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     const randomNum = Math.floor(Math.random() * 3);
     return choices[randomNum];
